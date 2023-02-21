da75a943e74342a50e0283e86a483775a4aa7135
Nxelia <nxelia99@gmail.com>
2023-02-21 18:51:57 +0100

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 //turns 
 const TURNS = {
@@ -21,16 +21,63 @@ const Square = ({children, updateBoard, index, isSelected}) => {
   )
 }
 
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 4, 7],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
 
 function App() {
+  //states
   //board (9 positions array), as a state
-
   const [board, setBoard] = useState(Array(9).fill(null))
   const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null)
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS){
+      const [a, b, c] = combo
+      if(
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ){return boardToCheck[a]}
+      
+    }
+    //in case there's no winner
+    return null
+  }
+  
 
   const updateBoard = (index) =>{
+    
+    //don't update the position if it already has a value
+    if(board[index] || winner) return
+    //spread and rest operator ***
+    // update the board and turns
+    const newBoard = structuredClone(board) //new board with props
+    newBoard[index] = turn // X u O when it's clicked
+    setBoard(newBoard) 
+    
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+
+    //check if there's a winner
+    //async
+    const newWinner = checkWinner(newBoard)
+    if (newWinner){
+      setWinner(() => {
+        console.log(`Ganador: ${newWinner}, el anterior era: ${previewQinner}`)
+        return newWinner
+      })
+    }
+  
   }
 
 
